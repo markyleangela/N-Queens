d742bea30bc160726428f9d3979fdc8120476ac7
markyleangela <markyleangela@gmail.com>
2024-10-08 09:09:04 +0800

    Add modified code that tries to solve the problem using the Local Beam algorithm

---
+++ Local-Beam/N Queens/N Queens/ACT4/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace ACT4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 16);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 222);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 247);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 155);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(265, 36);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(173, 100);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(448, 15);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(240, 222);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 262);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 49);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Step Forward";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(413, 263);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 48);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Just Solve It Already!!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(556, 247);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(265, 182);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(265, 16);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Possible moves";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(197, 262);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 49);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Randomize Queens";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 361);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "6-Queen Problem using Hill Climbing Algorithm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Local-Beam/N Queens/N Queens/ACT4/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Collections;
+
+namespace ACT4
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        int side;
+        int n = 6;
+        int k = 3;
+        SixState startState;
+        List<SixState> currentState;
+        int moveCounter;
+
+        //bool stepMove = true;
+
+        List<List<Point>> bMoves;
+        List<Point> chosenMove;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            side = pictureBox1.Width / n;
+
+            currentState = new List<SixState>();
+            //for(int i = 0; i < k; i++) currentState.Add(randomSixState());
+            currentState.Add(randomSixState());
+            currentState.Add(randomSixState());
+            currentState.Add(randomSixState());
+            startState = currentState[0];
+
+            updateUI();
+            label1.Text = "Attacking pairs: " + getAttackingPairs(startState);
+        }
+
+        private SixState randomSixState()
+        {
+            //SixState random = new SixState(r.Next(n),
+            //                                 r.Next(n),
+            //                                 r.Next(n),
+            //                                 r.Next(n),
+            //                                 r.Next(n),
+            //                                 r.Next(n));
+            //SixState random = new SixState(5, 5, 5, 5, 0, 1);
+            //SixState random = new SixState(5, 3, 3, 4, 4, 3);
+            SixState random = new SixState(2, 0, 4, 4, 5, 3);
+            return random;
+        }
+
+        private void updateUI()
+        {
+            //bMoves.Clear();
+            //pictureBox1.Refresh();
+            pictureBox2.Refresh();
+
+            //label1.Text = "Attacking pairs: " + getAttackingPairs(startState);
+            label3.Text = "Attacking pairs: " + getAttackingPairs(currentState[0]);
+            label4.Text = "Moves: " + moveCounter;
+            (int[,], int[,], int[,]) hTables = getHeuristicTableForPossibleMoves(currentState[0], currentState[1], currentState[2]);
+            bMoves = getBestMoves(hTables.Item1, hTables.Item2, hTables.Item3);
+
+
+            listBox1.Items.Clear();
+            foreach (Point move in bMoves[0])
+            {
+                listBox1.Items.Add(move);
+            }
+
+
+            if (bMoves[0].Count > 0) chosenMove = chooseMove(bMoves);
+            label2.Text = "Chosen move: " + chosenMove[0];
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // draw squares
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        e.Graphics.FillRectangle(Brushes.Blue, i * side, j * side, side, side);
+                    }
+                    // draw queens
+                    if (j == startState.Y[i]) e.Graphics.FillEllipse(Brushes.Fuchsia, i * side, j * side, side, side);
+                }
+            }
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            // draw squares
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        e.Graphics.FillRectangle(Brushes.Black, i * side, j * side, side, side);
+                    }
+                    // draw queens
+                    if (j == currentState[0].Y[i]) e.Graphics.FillEllipse(Brushes.Fuchsia, i * side, j * side, side, side);
+                }
+            }
+        }
+
+
+
+        private int getAttackingPairs(SixState f)
+        {
+            int attackers = 0;
+            
+            for (int rf = 0; rf < n; rf++)
+            {
+                for (int tar = rf+1; tar < n; tar++)
+                {
+                    // get horizontal attackers
+                    if (f.Y[rf] == f.Y[tar]) attackers++;
+                }
+                for (int tar = rf+1; tar < n; tar++)
+                {
+                    // get diagonal down attackers
+                    if (f.Y[tar] == f.Y[rf] + tar - rf) attackers++;
+                }
+                for (int tar = rf+1; tar < n; tar++)
+                {
+                    // get diagonal up attackers
+                    if (f.Y[rf] == f.Y[tar] + tar - rf) attackers++;
+                }
+            }
+            
+            return attackers;
+        }
+
+        private (int[,], int[,], int[,]) getHeuristicTableForPossibleMoves(SixState thisState1, SixState thisState2, SixState thisState3)
+        {
+            int[,] hStates1 = new int[n, n];
+            int[,] hStates2 = new int[n, n];
+            int[,] hStates3 = new int[n, n];
+
+            for (int i = 0; i < n; i++) // go through the indices
+            {
+                for (int j = 0; j < n; j++) // replace them with a new value
+                {
+                    SixState possible = new SixState(thisState1);
+                    possible.Y[i] = j;
+                    hStates1[i, j] = getAttackingPairs(possible);
+                }
+            }
+
+            for (int i = 0; i < n; i++) // go through the indices
+            {
+                for (int j = 0; j < n; j++) // replace them with a new value
+                {
+                    SixState possible = new SixState(thisState2);
+                    possible.Y[i] = j;
+                    hStates2[i, j] = getAttackingPairs(possible);
+                }
+            }
+
+            for (int i = 0; i < n; i++) // go through the indices
+            {
+                for (int j = 0; j < n; j++) // replace them with a new value
+                {
+                    SixState possible = new SixState(thisState3);
+                    possible.Y[i] = j;
+                    hStates3[i, j] = getAttackingPairs(possible);
+                }
+            }
+
+            return (hStates1, hStates2, hStates3);
+        }
+
+        private List<List<Point>> getBestMoves(int[,] heuristicTable1, int[,] heuristicTable2, int[,] heuristicTable3)
+        {
+            List<(int, Point)> bestMoves1 = new List<(int, Point)>();
+            List<(int, Point)> bestMoves2 = new List<(int, Point)>();
+            List<(int, Point)> bestMoves3 = new List<(int, Point)>();
+
+            int bestHeuristicValue = heuristicTable1[0, 0];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (bestHeuristicValue > heuristicTable1[i, j])
+                    {
+                        bestHeuristicValue = heuristicTable1[i, j];
+                        bestMoves1.Clear();
+                        if (currentState[0].Y[i] != j) bestMoves1.Add((bestHeuristicValue, new Point(i, j)));
+                    } else if (bestHeuristicValue == heuristicTable1[i,j])
+                    {
+                        if (currentState[0].Y[i] != j) bestMoves1.Add((bestHeuristicValue, new Point(i, j)));
+                    }
+                }
+            }
+
+            bestHeuristicValue = heuristicTable2[0, 0];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (bestHeuristicValue > heuristicTable2[i, j])
+                    {
+                        bestHeuristicValue = heuristicTable2[i, j];
+                        bestMoves2.Clear();
+                        if (currentState[1].Y[i] != j) bestMoves2.Add((bestHeuristicValue, new Point(i, j)));
+                    }
+                    else if (bestHeuristicValue == heuristicTable2[i, j])
+                    {
+                        if (currentState[1].Y[i] != j) bestMoves2.Add((bestHeuristicValue, new Point(i, j)));
+                    }
+                }
+            }
+
+            bestHeuristicValue = heuristicTable3[0, 0];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (bestHeuristicValue > heuristicTable3[i, j])
+                    {
+                        bestHeuristicValue = heuristicTable3[i, j];
+                        bestMoves3.Clear();
+                        if (currentState[2].Y[i] != j) bestMoves3.Add((bestHeuristicValue, new Point(i, j)));
+                    }
+                    else if (bestHeuristicValue == heuristicTable3[i, j])
+                    {
+                        if (currentState[2].Y[i] != j) bestMoves3.Add((bestHeuristicValue, new Point(i, j)));
+                    }
+                }
+            }
+            label5.Text = "Possible Moves (H="+bestHeuristicValue+")";
+
+            List<Point> moves1 = bestMoves1.OrderBy(x => x.Item1).ToList().Select(x => x.Item2).ToList();
+            List<Point> moves2 = bestMoves2.OrderBy(x => x.Item1).ToList().Select(x => x.Item2).ToList();
+            List<Point> moves3 = bestMoves3.OrderBy(x => x.Item1).ToList().Select(x => x.Item2).ToList();
+
+            List<List<Point>> combinedMoves = new List<List<Point>>();
+            combinedMoves.Add(moves1);
+            combinedMoves.Add(moves2);
+            combinedMoves.Add(moves3);
+            return combinedMoves;
+        }
+
+        private List<Point> chooseMove(List<List<Point>> possibleMoves)
+        {
+            List<Point> moves = new List<Point>();
+            moves.Add(possibleMoves[0][0]);
+            moves.Add(possibleMoves[1][0]);
+            moves.Add(possibleMoves[2][0]);
+            return moves;
+        }
+
+        private void executeMove(List<Point> moves)
+        {
+            for (int i = 0; i < n; i++) startState.Y[i] = currentState[0].Y[i];
+            currentState[0].Y[moves[0].X] = moves[0].Y;
+
+            //for (int i = 0; i < n; i++) startState.Y[i] = currentState[1].Y[i];
+            currentState[1].Y[moves[1].X] = moves[1].Y;
+
+            //for (int i = 0; i < n; i++) startState.Y[i] = currentState[2].Y[i];
+            currentState[2].Y[moves[2].X] = moves[2].Y;
+
+            moveCounter++;
+
+            //chosenMove = null;
+            updateUI();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (getAttackingPairs(currentState[0]) > 0) executeMove(chosenMove);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentState.Clear();
+            for (int i = 0; i < k; i++) currentState.Add(randomSixState());
+            startState = currentState[0];
+
+            moveCounter = 0;
+
+            updateUI();
+            pictureBox1.Refresh();
+            label1.Text = "Attacking pairs: " + getAttackingPairs(startState);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x1 = getAttackingPairs(currentState[0]);
+            int x2 = getAttackingPairs(currentState[1]);
+            int x3 = getAttackingPairs(currentState[2]);
+            while (true)
+            {
+                if (x1 <= 0 || x2 <= 0 || x3 <= 0) break;
+
+                executeMove(chosenMove);
+                x1 = getAttackingPairs(currentState[0]);
+                x2 = getAttackingPairs(currentState[1]);
+                x3 = getAttackingPairs(currentState[2]);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
